9c1e60a5d1beb94cd0b040000cebbfa3f8d2adb8
OleksandrKushnir-st <Oleksandr.kushnir20@gmail.com>
2025-05-06 20:51:15 +0000

    imp: maps mancano un po di logiche ma la base funziona

--- MaDe/src/app/app.component.ts
+++ MaDe/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalendarioComponent } from './calendario/calendario.component';
+import { MapsComponent } from './components/maps/maps.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CalendarioComponent],
+  imports: [RouterOutlet, CalendarioComponent,MapsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
