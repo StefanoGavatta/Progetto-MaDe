238c202fd7691373b33a369c40e3313ddf868969
StefanoGavatta <144794088+StefanoGavatta@users.noreply.github.com>
2025-05-07 07:43:35 +0000

    introduzione con bootstrap

--- MaDe/src/app/app.component.ts
+++ MaDe/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalendarioComponent } from './components/calendario/calendario.component';
 import { MapsComponent } from './components/maps/maps.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CalendarioComponent,MapsComponent],
+  imports: [RouterOutlet, CalendarioComponent,MapsComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
