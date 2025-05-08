608388be643c84fa87e7284760b2bc21d0aaf500
StefanoGavatta <144794088+StefanoGavatta@users.noreply.github.com>
2025-05-08 11:38:02 +0000

    video

--- MaDe/src/app/components/scuola-specifica/scuola-specifica.component.ts
+++ MaDe/src/app/components/scuola-specifica/scuola-specifica.component.ts
@@ -4,10 +4,11 @@ import { ImageCarouselComponent } from './image-carousel/image-carousel.componen
 import { ChiSiamoComponent } from './chi-siamo/chi-siamo.component';
 import { InfoComponent } from './info/info.component';
 import { EventiComponent } from './eventi/eventi.component';
+import { VideoComponent } from './video/video.component';
 
 @Component({
   selector: 'app-scuola-specifica',
-  imports: [ImageCarouselComponent,ChiSiamoComponent, InfoComponent, EventiComponent],
+  imports: [ImageCarouselComponent,ChiSiamoComponent, InfoComponent, EventiComponent, VideoComponent],
   templateUrl: './scuola-specifica.component.html',
   styleUrl: './scuola-specifica.component.css'
 })
