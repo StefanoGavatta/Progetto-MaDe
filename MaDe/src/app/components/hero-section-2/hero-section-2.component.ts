78284650c64b86670994f832b7a82d0b74616298
StefanoGavatta <144794088+StefanoGavatta@users.noreply.github.com>
2025-05-21 19:01:32 +0000

    commit

--- MaDe/src/app/components/hero-section-2/hero-section-2.component.ts
+++ MaDe/src/app/components/hero-section-2/hero-section-2.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero-section-2',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './hero-section-2.component.html',
-  styleUrl: './hero-section-2.component.css'
+  styleUrl: './hero-section-2.component.css',
+  standalone: true
 })
 export class HeroSection2Component {
 
