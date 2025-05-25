8d6c5b78dd4eecc317aa4ef4b8b57c3ed228f5c3
OleksandrKushnir-st <oleksandr.kushnir20@gmail.com>
2025-05-25 21:39:17 +0200

    /

--- MaDe/src/app/components/pagina-mappa/maps/maps.component.ts
+++ MaDe/src/app/components/pagina-mappa/maps/maps.component.ts
@@ -48,7 +48,7 @@ export class MapsComponent implements AfterViewInit, OnInit {
   scuolaData: WritableSignal<SchoolsData | null> = signal(null);
  defaultIcon = new L.Icon({
   iconUrl: "/verde.png",
-  iconSize: [80, 41],
+  iconSize: [120, 80],
   iconAnchor: [10, 41],
   popupAnchor: [2, -40]
 });
@@ -64,7 +64,7 @@ export class MapsComponent implements AfterViewInit, OnInit {
 
   private defaultIconPuntatori: Icon = icon({
     iconUrl: "/blue.png",
-    iconSize: [20, 30], // Imposta la larghezza e l'altezza desiderate (es. 32x32 pixel)
+    iconSize: [40, 60], // Imposta la larghezza e l'altezza desiderate (es. 32x32 pixel)
     iconAnchor: [16, 32], // Imposta il punto dell'icona che corrisponde alla posizione (es. il centro inferiore)
     popupAnchor: [0, -32]  // Imposta dove si "aggancia" la popup rispetto all'icona (es. sopra il centro)
   });
